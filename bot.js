4b384a32870e615ec4b994084336538b7c4ef6c2
diogoscf <diogo.scf2003@gmail.com>
2020-05-09 17:12:49 +0200

    Add commands

--- bot.js
+++ bot.js
@@ -9,6 +9,41 @@ const { TOKEN } = process.env;
 const bot = new DiscordBot.Client();
 bot.login(TOKEN);
 
+const PREFIX = ";";
+
+const COMMANDS = {
+  help: (msg) => handleHelp(msg),
+  about: (msg) => handleAbout(msg),
+  invite: (msg) => handleTopgg(msg),
+  topgg: (msg) => handleTopgg(msg),
+  stats: (msg) => handleAbout(msg),
+  ping: (msg) => handlePing(msg)
+};
+
+function convertMS(milliseconds) {
+  let days, hours, minutes, seconds;
+  seconds = Math.floor(milliseconds / 1000);
+  minutes = Math.floor(seconds / 60);
+  seconds = (seconds % 60).toString();
+  hours = Math.floor(minutes / 60);
+  minutes = (minutes % 60).toString();
+  days = Math.floor(hours / 24);
+  hours = (hours % 24).toString();
+
+  if (hours.length === 1) hours = "0" + hours;
+  if (minutes.length === 1) minutes = "0" + minutes;
+  if (seconds.length === 1) seconds = "0" + seconds;
+
+  let dayStr = "";
+  if (days > 1) {
+    dayStr = `${days} days, `
+  } else if (days === 1) {
+    dayStr = "1 day, "
+  }
+
+  return `${dayStr}${hours}:${minutes}:${seconds}`
+}
+
 function formatIndent(str) {
   const lines = str.replace(/\t/g, "    ").split("\n");
   let ignored = [];
@@ -34,21 +69,13 @@ function formatIndent(str) {
   return newLines.join("\n");
 };
 
-bot.on("message", async (msg) => {
-
-  // prevent replying to own messages
-  if (msg.author.id == bot.user.id) {
-    return;
-  };
-
-  const githubMatch = msg.content.match(/https?:\/\/github.com\/([a-zA-Z0-9-_]+\/[A-Za-z0-9_.-]+)\/blob\/(.+)#L(\d+)-?L?(\d*)/i);
-  if (!githubMatch) return;
-
+async function handleGithub(msg, githubMatch) {
   msg.suppressEmbeds(true);
 
   const resp = await fetch(`https://raw.githubusercontent.com/${githubMatch[1]}/${githubMatch[2]}`);
   const text = await resp.text();
   const lines = text.split("\n");
+
   let toDisplay;
   if (!githubMatch[4].length) {
     toDisplay = lines[parseInt(githubMatch[3], 10) - 1].trim();
@@ -56,10 +83,107 @@ bot.on("message", async (msg) => {
     toDisplay = formatIndent(lines.slice(parseInt(githubMatch[3], 10) - 1, parseInt(githubMatch[4], 10)).join("\n"));
   };
 
-  const extension = githubMatch[2].includes(".") ? githubMatch[2].split(".") : [""]
+  const extension = githubMatch[2].includes(".") ? githubMatch[2].split(".") : [""];
 
   msg.channel.send(`\`\`\`${extension[extension.length - 1]}\n${toDisplay}\`\`\``);
 
   setTimeout(() => msg.suppressEmbeds(true), 2000); // make sure to suppress the embed
+};
 
-})
+async function handleAbout(msg) {
+  const botApp = await bot.fetchApplication();
+  const aboutEmbed = new DiscordBot.MessageEmbed()
+    .setTitle("About GitHub Lines")
+    .setDescription("GitHub Lines is a bot that displays one or more lines when mentioned in a GitHub link")
+    //.setThumbnail("IMAGE HERE")
+    .addFields({
+      name: "**__Info__**",
+      value: "\u200b",
+      inline: false
+    }, {
+      name: "Guild Count",
+      value: bot.guilds.cache.size,
+      inline: true
+    }, {
+      name: "User Count",
+      value: bot.users.cache.size,
+      inline: true
+    }, {
+      name: "Uptime",
+      value: convertMS(bot.uptime),
+      inline: true
+    }, {
+      name: "Latency",
+      value: `${bot.ws.ping}ms`,
+      inline: true
+    })
+    .setFooter(`Made by ${botApp.owner.tag}`, botApp.owner.avatarURL());
+
+  msg.channel.send(aboutEmbed);
+}
+
+function handleTopgg(msg) {
+  msg.reply("https://top.gg/bot/708282735227174922");
+}
+
+function handleHelp(msg) {
+  const helpEmbed = new DiscordBot.MessageEmbed()
+    .setTitle("Help Info")
+    .setDescription("GitHub Lines runs automatically, without need for configuration! Here are some commands you can use")
+    //.setThumbnail("IMAGE HERE")
+    .addFields({
+      name: "**__Commands__**",
+      value: "\u200b",
+      inline: false
+    }, {
+      name: "`;about`",
+      value: "Info about the bot",
+      inline: false
+    }, {
+      name: "`;invite` or `;topgg`",
+      value: "Link to the bot's top.gg page",
+      inline: false
+    }, {
+      name: "`;help`",
+      value: "Return this message",
+      inline: false
+    }, {
+      name: "`;ping`",
+      value: "Check bot latency",
+      inline: false
+    });
+
+  msg.channel.send(helpEmbed);
+}
+
+async function handlePing(msg) {
+  pingMsg = await msg.channel.send("Ping?");
+  pingMsg.edit(`Pong! Latency is ${pingMsg.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${bot.ws.ping}ms`);
+};
+
+bot.on("ready", () => {
+  bot.user.setActivity("for GitHub links", {
+      type: "WATCHING"
+    });
+});
+
+bot.on("message", async (msg) => {
+  // prevent replying to own messages
+  if (msg.author.id == bot.user.id) {
+    return;
+  };
+
+  const githubMatch = msg.content.match(/https?:\/\/github.com\/([a-zA-Z0-9-_]+\/[A-Za-z0-9_.-]+)\/blob\/(.+)#L(\d+)-?L?(\d*)/i);
+  if (githubMatch) handleGithub(msg, githubMatch);
+
+  let command = "INVALID-COMMAND";
+  if (msg.content.trim().startsWith(`<@!${bot.user.id}>`)) {
+    command = msg.content.substring(`<@!${bot.user.id}>`.length).trim();
+  } else if (msg.content.trim().startsWith(PREFIX)) {
+    command = msg.content.substring(PREFIX.length).trim();
+  };
+
+  if (Object.keys(COMMANDS).includes(command)) {
+    COMMANDS[command](msg);
+  };
+});
